611ae6f0d054b7871ca747e6a3c40d41d26b36eb
Maxim Volgin <maxvol@users.noreply.github.com>
2020-07-01 19:53:13 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,28 @@
+  
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "JSONLayout",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+    ],
+    products: [
+        .library(
+            name: "JSONLayout",
+            targets: ["JSONLayout"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "JSONLayout",
+            dependencies: [],
+            path: "Sources"),
+//        .testTarget(
+//            name: "JSONLayoutTests",
+//            dependencies: ["JSONLayout"],
+//            path: "Tests"
+//        )
+    ]
+)
